e5f8ea5adc1d59e9a2a67c3492ac2e42cd056368
Dark-jin <chch4lee1@naver.com>
2023-01-29 17:40:32 +0900

    feat: kakao javascript

--- src/router/routers.tsx
+++ src/router/routers.tsx
@@ -1,8 +1,16 @@
-import { Main } from '../pages';
+import { Main } from "../pages";
+import Share from "../pages/Sharepage";
+
 const Router = [
   {
-    title: 'Home',
-    url: '/',
+    title: "Home",
+    url: "/",
     component: <Main />,
   },
+  {
+    title: "카카오톡 공유하기",
+    url: "Sharepage",
+    component: <Share />,
+  },
 ];
+export default Router;
